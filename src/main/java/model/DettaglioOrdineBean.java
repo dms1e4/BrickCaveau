f17e21576853a8efc7588356a1cc4289dcd4913e
robertorega <r.rega24@studenti.unisa.it>
2026-06-05 16:05:37 +0200

    Creato DettaglioOrdineBean.java

---
+++ src/main/java/model/DettaglioOrdineBean.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+public class DettaglioOrdineBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private int ordineId;
+    private Integer codiceSet;
+    private int quantita;
+    private double prezzoAcquisto;
+    private double iva;
+
+    public DettaglioOrdineBean() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getOrdineId() { return ordineId; }
+    public void setOrdineId(int ordineId) { this.ordineId = ordineId; }
+
+    public Integer getCodiceSet() { return codiceSet; }
+    public void setCodiceSet(Integer codiceSet) { this.codiceSet = codiceSet; }
+
+    public int getQuantita() { return quantita; }
+    public void setQuantita(int quantita) { this.quantita = quantita; }
+
+    public double getPrezzoAcquisto() { return prezzoAcquisto; }
+    public void setPrezzoAcquisto(double prezzoAcquisto) { this.prezzoAcquisto = prezzoAcquisto; }
+
+    public double getIva() { return iva; }
+    public void setIva(double iva) { this.iva = iva; }
+}
