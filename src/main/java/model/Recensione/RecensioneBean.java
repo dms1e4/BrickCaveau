68e248c1e72193379ffc501af8ea26a91ccfbe07
robertorega <r.rega24@studenti.unisa.it>
2026-06-05 16:09:03 +0200

    Creato RecensioneBean.java

---
+++ src/main/java/model/Recensione/RecensioneBean.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class RecensioneBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int utenteId;
+    private int codiceSet;
+    private int rating;
+    private String testo;
+    private Date dataRecensione;
+
+    // Campi per front-end
+    private String nomeUtente;
+    private String cognomeUtente;
+
+    public RecensioneBean() {}
+
+    public int getUtenteId() { return utenteId; }
+    public void setUtenteId(int utenteId) { this.utenteId = utenteId; }
+
+    public int getCodiceSet() { return codiceSet; }
+    public void setCodiceSet(int codiceSet) { this.codiceSet = codiceSet; }
+
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
+
+    public String getTesto() { return testo; }
+    public void setTesto(String testo) { this.testo = testo; }
+
+    public Date getDataRecensione() { return dataRecensione; }
+    public void setDataRecensione(Date dataRecensione) { this.dataRecensione = dataRecensione; }
+
+    public String getNomeUtente() { return nomeUtente; }
+    public void setNomeUtente(String nomeUtente) { this.nomeUtente = nomeUtente; }
+
+    public String getCognomeUtente() { return cognomeUtente; }
+    public void setCognomeUtente(String cognomeUtente) { this.cognomeUtente = cognomeUtente; }
+}
