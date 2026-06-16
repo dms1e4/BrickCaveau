60adf69e7b14649814668c1256b21dcd2e6f9a38
dms1e4 <simone.esposito4@proton.me>
2026-06-16 17:30:27 +0200

    Update LogoutServlet.java

--- src/main/java/control/LogoutServlet.java
+++ src/main/java/control/LogoutServlet.java
@@ -24,7 +24,7 @@ public class LogoutServlet extends HttpServlet {
             session.invalidate(); 
         }
         
-        response.sendRedirect(request.getContextPath() + "/index.jsp");
+        response.sendRedirect(request.getContextPath() + "/Home");
     }
 
     @Override
